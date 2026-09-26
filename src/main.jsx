db2d349ce87f98bf7e0001bff4a818c98c82d95e
ismailalpha1 <ijalali626@gmail.com>
2026-09-26 19:39:47 +0200

    Add meta balise

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <meta name="p:domain_verify" content="02675f34a3c97f8206aea8e8cd243607"/>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
